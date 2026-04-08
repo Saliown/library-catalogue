7a84a05255d95dba11106fc35551580c6aa10355
Sali <sydykovasalima543@gmail.com>
2026-04-08 17:31:56 +0600

    Добавили эндпоинты на поиск, добавление книг и запрос по ISBN

--- library.mjs
+++ library.mjs
@@ -48,7 +48,7 @@ async function disconnect() {
 async function addGenre(args) {
     let query = `insert into GENRE(TYPE)
         output inserted.id
-        values ('${args[0]}');`
+        request.input("name", sql.VarChar, args[0])`
 
     try {
         // создаем и выполняем запрос
@@ -62,30 +62,42 @@ async function addGenre(args) {
     }
 }
 
-async function addAuthor(args) {
+async function addAuthor(authorName) {
     let query = `insert into AUTHORS(NAME_A)
-        output inserted.ID
-        values ('${args[0]}');`;
+        output inserted.id
+        values(@name)`;
 
     try {
         const request = new sql.Request();
-        const result = await request.query(query);
-        console.log("Автор добавлен, id:", result.recordset[0].ID);
+        const result = await request
+            .input("name", sql.VarChar, authorName)
+            .query(query);
+        
         return result.recordset[0].id;
     } catch (e) {
         console.log("Ошибка:", e.message);
     }
 }
 
+async function addOrFindAuthor(authorName) {
+    let id = await findAuthor(authorName);
+
+    if (id == null)
+        id = await addAuthor(authorName);
+
+    return id;
+}
+
+
 async function findAuthor(name) {
     let query = `
         select id from AUTHORS
-        where name_a = '${name}';
+        where name_a = @name;
     `;
 
     try {
         const request = new sql.Request();
-        const result = await request.query(query);
+        const result = await request.input("name", name).query(query);
         return result.recordset[0].id;
     } catch (e) {
         console.log("Ошибка в findAuthor:", e.message);
@@ -181,5 +193,98 @@ if (process.argv.length > 2) {
     process.exit();
 }
 else {
-    app.listen(3000);
+    await connect();
+    app.listen(3000, () => console.log("ЖДУ"));
 }
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    let query = `
+        SELECT * FROM USERS
+        WHERE EMAIL = @Email AND PASSWORD = @Password
+    `;
+
+    const request = new sql.Request();
+    request.input("Email", sql.VarChar, email);
+    request.input("Password", sql.VarChar, password);
+
+    const result = await request.query(query);
+
+    if (result.recordset.length === 0) {
+        return res.status(401).json({ message: "Неверные данные" });
+    }
+
+    res.json(result.recordset[0]);
+});
+
+// ищем книжки по ISBN
+app.post("/query-isbn", async (req, res) => {
+    const request = new sql.Request();
+    let query = 
+    `select name_a, title, year, total from
+        books inner join authors on books.AUTHOR_ID = authors.id
+            left join amounts on amounts.isbn = books.isbn
+    where books.isbn = @isbn
+    `;
+
+    const result = await request.input("isbn", req.body.isbn).query(query);
+
+    // такие книжки уже есть
+    if (result.recordset.length > 0) {
+        res.status(200).json(result.recordset[0]);
+    }
+    else {
+        res.status(404).send();
+    }
+});
+
+
+// внесем новые книжки в базу за один заход
+app.post("/add-books", async(req,res) => {
+    // сначала ищем такие книжки по ISBN
+
+    let authorId = await addOrFindAuthor(req.body.author);
+
+    let request = new sql.Request();
+    let query = `insert into books(isbn, author_id, title, year)
+        output inserted.isbn
+        values(@isbn, @author_id, @title, @year)`;
+    
+    let result = await request
+            .input("isbn", req.body.isbn)
+            .input("author_id", authorId)
+            .input("title", req.body.title)
+            .input("year", req.body.year)
+            .query(query);
+    
+    let isbn = result.recordset[0].isbn;
+
+    request = new sql.Request();
+    query = `insert into amounts (isbn, total) output inserted.total values (@isbn, @total)`;
+    result = await request
+        .input("isbn", isbn)
+        .input("total", req.body.amount)
+        .query(query);
+    
+    let total = result.recordset[0].total;
+
+    res.status(200).json({isbn, total});
+
+});
+
+app.post("/search", async (req, res) => {
+    let query = `
+        select isbn, name_a, title
+        from books inner join authors on books.author_id = authors.id
+        where name_a like @author and title like @title
+    `;
+
+    let request = new sql.Request();
+    let result = await request
+        .input("author", req.body.author)
+        .input("title", req.body.title)
+        .query(query);
+    
+    res.status(200).json(result.recordset);
+});
